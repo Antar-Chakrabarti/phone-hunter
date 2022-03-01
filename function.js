519b17eeb008b4d0ed36de76ce3dc44a4c872c90
Antar-chakrabarti <chakrabartiantar@gmail.com>
2022-03-01 14:04:39 +0600

    six number commit

--- function.js
+++ function.js
@@ -1,30 +1,30 @@
-// handle click event
-document.getElementById('search-btn').addEventListener('click', () => {
-    const phnText = document.getElementById('input-txt').value.toLowerCase();
-    if (phnText === '') {
-        document.getElementById('search-warning').style.display = 'block';
-        return '';
-    } else {
-        document.getElementById('search-warning').style.display = 'none';
-    }
-    const url = `https://openapi.programming-hero.com/api/phones?search=${phnText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data.slice(0, 20)))
-});
-// displaying data
-const displayPhone = detail => {
-    if (detail.length == '') {
-        document.getElementById('worn-result').style.display = 'block';
-    } else {
-        document.getElementById('worn-result').style.display = 'none';
-    }
-    const parant = document.getElementById('card-section');
-    parant.textContent = '';
-    for (const details of detail) {
-        const div = document.createElement('col');
-        div.classList.add('col');
-        div.innerHTML = `                
+ // handle click event
+ document.getElementById('search-btn').addEventListener('click', () => {
+     const phnText = document.getElementById('input-txt').value.toLowerCase();
+     if (phnText === '') {
+         document.getElementById('search-warning').style.display = 'block';
+         return '';
+     } else {
+         document.getElementById('search-warning').style.display = 'none';
+     }
+     const url = `https://openapi.programming-hero.com/api/phones?search=${phnText}`
+     fetch(url)
+         .then(res => res.json())
+         .then(data => displayPhone(data.data.slice(0, 20)))
+ });
+ // displaying data
+ const displayPhone = detail => {
+     if (detail.length == '') {
+         document.getElementById('worn-result').style.display = 'block';
+     } else {
+         document.getElementById('worn-result').style.display = 'none';
+     }
+     const parant = document.getElementById('card-section');
+     parant.textContent = '';
+     for (const details of detail) {
+         const div = document.createElement('col');
+         div.classList.add('col');
+         div.innerHTML = `                
         <div class="col">
           <div class="card">
               <img src="${details.image}" class="card-img-top" alt="...">
@@ -37,23 +37,23 @@ const displayPhone = detail => {
           </div>
       </div>
     `
-        parant.appendChild(div);
-    }
-    // console.log(detail)
-}
+         parant.appendChild(div);
+     }
+     // console.log(detail)
+ }
 
-const showDetails = (ProductId) => {
-    const url = `https://openapi.programming-hero.com/api/phone/${ProductId}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayMobileDetails(data))
-        //console.log(ProductId);
-}
+ const showDetails = (ProductId) => {
+     const url = `https://openapi.programming-hero.com/api/phone/${ProductId}`
+     fetch(url)
+         .then(res => res.json())
+         .then(data => displayMobileDetails(data))
+         //console.log(ProductId);
+ }
 
-const displayMobileDetails = data => {
-    const parant = document.getElementById('details-section');
-    if (data.data.releaseDate == '') {
-        parant.innerHTML = `
+ const displayMobileDetails = data => {
+     const parant = document.getElementById('details-section');
+     if (data.data.releaseDate == '') {
+         parant.innerHTML = `
         <div class="card" style="width: 18rem;">
         <img src="${data.data.image}" class="card-img-top">
         <div class="card-body">
@@ -74,10 +74,18 @@ const displayMobileDetails = data => {
         ${data.data.mainFeatures.sensors[4]},
         ${data.data.mainFeatures.sensors[5]}
         </p>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item"><b>Others Fitures: </b></li>
+        <li class="list-group-item">Blouetooth: ${data.data.others.Bluetooth}</li>
+        <li class="list-group-item">GPS: ${data.data.others.GPS}</li>
+        <li class="list-group-item">NFC: ${data.data.others.NFC}</li>
+        <li class="list-group-item">Radio: ${data.data.others.Radio}</li>
+        <li class="list-group-item">WLAN: ${data.data.others.WLAN}</li>
+        </ul>
     </div>
         `
-    } else {
-        parant.innerHTML = `
+     } else {
+         parant.innerHTML = `
         <div class="card" style="width: 18rem;">
         <img src="${data.data.image}" class="card-img-top">
         <div class="card-body">
@@ -98,12 +106,19 @@ const displayMobileDetails = data => {
         ${data.data.mainFeatures.sensors[4]},
         ${data.data.mainFeatures.sensors[5]}
         </p>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item"><b>Others Fitures: </b></li>
+        <li class="list-group-item">Blouetooth: ${data.data.others.Bluetooth}</li>
+        <li class="list-group-item">GPS: ${data.data.others.GPS}</li>
+        <li class="list-group-item">NFC: ${data.data.others.NFC}</li>
+        <li class="list-group-item">Radio: ${data.data.others.Radio}</li>
+        <li class="list-group-item">WLAN: ${data.data.others.WLAN}</li>
+        </ul>
     </div>
-        `
-    }
 
-    //console.log(data.data.mainFeatures)
-    console.log(data);
+      `
+     }
 
-    console.log(data.data.mainFeatures.chipSet)
-}
+     //console.log(data.data.mainFeatures)
+     // console.log(data);
+ }
